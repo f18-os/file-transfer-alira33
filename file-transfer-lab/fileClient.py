60e4a6fad523abe766f6afaf11e6625ebb78cae9
abigail.lira <al.abigail.lira@gmail.com>
2018-10-01 04:17:50 +0100

    multiple clients accepted

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -1,7 +1,7 @@
 import socket
  
-csFT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-csFT.connect((socket.gethostname(), 50001))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 50001))
  
 text_file = 'input.txt'
  
@@ -9,27 +9,27 @@ text_file = 'input.txt'
 with open(text_file, 'rb') as fs:
     #Using with, no file close is necessary,
     #with automatically handles file close
-    csFT.send(b'BEGIN')
+    s.send(b'BEGIN')
     while True:
         data = fs.read(1024)
         print('Sending data', data.decode('utf-8'))
-        csFT.send(data)
+        s.send(data)
         print('Sent data', data.decode('utf-8'))
         if not data:
             print('Breaking from sending data')
             break
-    csFT.send(b'ENDED')
+    s.send(b'ENDED')
     fs.close()
  
 #Receive file
 print("Receiving..")
 with open(text_file, 'wb') as fw:
     while True:
-        data = csFT.recv(1024)
+        data = s.recv(1024)
         if not data:
             break
         fw.write(data)
     fw.close()
 print("Received..")
  
-csFT.close()
+s.close()
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -1,45 +1,66 @@
-import socket
- 
-ssFT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ssFT.bind((socket.gethostname(), 50001))
-ssFT.listen(1)
-while True:
-    (conn, address) = ssFT.accept()
-    text_file = 'fileProj.txt'
- 
-    #Receive, output and save file
-    with open(text_file, "wb") as fw:
-        print("Receiving..")
-        while True:
-            print('receiving')
-            data = conn.recv(100)
-            if data == b'BEGIN':
-                continue
-            elif data == b'ENDED':
-                print('Breaking from file write')
+import socket, os
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 50001))
+s.listen(10)  # multiple clients
+
+def handle_client(s, addr, i, c):
+    while True:
+        text_file = 'fileProj.txt'
+
+        # Receive, output and save file
+        with open(text_file, "wb") as fw:
+            print("Receiving..")
+            while True:
+                print('receiving')
+                data = c.recv(100)
+                if data == b'BEGIN':
+                    continue
+                elif data == b'ENDED':
+                    print('Breaking from file write')
+                    break
+                else:
+                    print('Received: ', data.decode('utf-8'))
+                    fw.write(data)
+                    print('Wrote to file', data.decode('utf-8'))
+            fw.close()
+            print("Received..")
+            decoded_data = data.decode("utf-8")
+            if not decoded_data:
+                print("\nconnection with client " + str(i) + " broken\n")
                 break
-            else:
-                print('Received: ', data.decode('utf-8'))
-                fw.write(data)
-                print('Wrote to file', data.decode('utf-8'))
-        fw.close()
-        print("Received..")
- 
-    #Append and send file
-    print('Opening file ', text_file)
-    with open(text_file, 'ab+') as fa:
-        print('Opened file')
-        print("Appending string to file.")
-        string = b"Append this to file."
-        fa.write(string)
-        fa.seek(0, 0)
-        print("Sending file.")
-        while True:
-            data = fa.read(1024)
-            conn.send(data)
-            if not data:
-                break
-        fa.close()
-        print("Sent file.")
-    break
-ssFT.close()
+            print("  CLIENT " + str(i) + " -> " + decoded_data)
+
+        # Append and send file
+        print('Opening file ', text_file)
+        with open(text_file, 'ab+') as fa:
+            print('Opened file')
+            print("Appending string to file.")
+            string = b"Append this to file."
+            fa.write(string)
+            fa.seek(0, 0)
+            print("Sending file.")
+            while True:
+                data = fa.read(1024)
+                conn.send(data)
+                if not data:
+                    break
+            fa.close()
+            print("Sent file.")
+        break
+
+def server():
+    i = 1
+    while i <= 10:
+        c, addr = s.accept()
+        child_pid = os.fork()
+        if child_pid == 0:
+            print("\nconnection successful with client " +
+                  str(i) + str(addr) + "\n")
+            handle_client(c, addr, i, c)
+        else:
+            i += 1
+
+
+server()
+s.close()
